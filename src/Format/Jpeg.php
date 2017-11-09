d92c1c9baa9a3ecaf2f03debfb4a199ec4c31abc
Roger Russel <roger.neris@locaweb.com.br>
2017-11-09 15:13:36 -0200

    image runing

--- src/Format/Jpeg.php
+++ src/Format/Jpeg.php
@@ -11,9 +11,10 @@
      $compress_command = GUETZLI . " --quality " . self::GUETZLI_QUALITY . " $full_image_path $full_image_path.compressed";
      exec($compress_command);
 
-     if(!is_file($full_image_path .'.compressed'))
-       $recompress= JPEGRECOMPRESS . " --quiet --accurate --quality high --min 80 -m ms-ssim $full_image_path $full_image_path.compressed";
+     if(!is_file($full_image_path .'.compressed')){
+       $recompress = JPEGRECOMPRESS . " --quiet --accurate --quality high --min 80 -m ms-ssim $full_image_path $full_image_path.compressed";
        exec($recompress);
+     }
 
      if(is_file($full_image_path .'.compressed'))
        rename($full_image_path .'.compressed', $full_image_path);
