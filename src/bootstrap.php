a793c1508be31d8d9a26e49e4504baaa8ab8457e
Roger Russel <roger.neris@locaweb.com.br>
2017-11-08 22:07:44 -0200

    fix const declaration

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,5 +1,6 @@
 <?php
 
-CONST __OPT__ = realpath(__DIR__ . '/../') . '/opt';
+define('__OPT__', realpath(__DIR__ . '/../') . '/opt' );
+
 CONST GUETZLI = __OPT__ . '/guetzli/bin/guetzli';
 CONST JPEGRECOMPRESS = __OPT__ . '/jpeg-archive/jpeg-recompress';
